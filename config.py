e549df19b28c9de997220bd7a7f87b54e5d30251
Karson Cheng <kaho.career@gmail.com>
2016-11-04 11:27:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'FyD0vXTgvxne86w4rtoItnBHO'
+consumer_secret = 'I7rEUW08Sktk89X3Vae8iXH5lstOQAU0Lz6560uXRdAGDMTFeW'
+access_token = '3002005388-ilm6MbJEq9SGMS9uSdCmUh4d17MlxyuZnvFaoxy'
+access_secret = 'bJ4dOpI7LzTrjCWcWzuaOtXp9Z0KPe63sdynXMWQSAwWp'
